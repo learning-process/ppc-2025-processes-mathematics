c9f8749865c11a6611eb8d48249d1affb81ec97d
banp2892 <sseva2892@gmail.com>
2025-10-28 20:31:36 +0000

    napisal cpp ops_mpi, nado budet potestit

--- tasks/chernykh_s_min_matrix_elements/common/include/common.hpp
+++ tasks/chernykh_s_min_matrix_elements/common/include/common.hpp
@@ -8,9 +8,9 @@
 
 namespace chernykh_s_min_matrix_elements {
 
-using InType = std::tuple<int,int,std::vector<double>>;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::tuple<size_t,size_t,std::vector<double>>;
+using OutType = double;
+using TestType = std::string;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace chernykh_s_min_matrix_elements

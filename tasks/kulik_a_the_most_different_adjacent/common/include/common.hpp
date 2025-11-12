69a8bb52fb57a36968db4c9c4b430f2db4fdef3d
Mamontyonok <kulikartur05@yandex.ru>
2025-11-12 15:44:14 +0300

    Update common.hpp

--- tasks/kulik_a_the_most_different_adjacent/common/include/common.hpp
+++ tasks/kulik_a_the_most_different_adjacent/common/include/common.hpp
@@ -2,14 +2,15 @@
 
 #include <string>
 #include <tuple>
+#include <vector>
 
 #include "task/include/task.hpp"
 
 namespace kulik_a_the_most_different_adjacent {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::vector<double>;
+using OutType = std::pair<int, int>;
+using TestType = std::string;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace kulik_a_the_most_different_adjacent

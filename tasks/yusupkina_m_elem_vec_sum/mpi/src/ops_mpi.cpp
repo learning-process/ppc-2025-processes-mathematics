f6bac9ab03301d6ca1ab882ca686a0959d7da593
Rita Yusupkina <yusupkina.rita@gmail.com>
2025-11-26 04:29:05 +0000

    mpi version fix

--- tasks/yusupkina_m_elem_vec_sum/mpi/src/ops_mpi.cpp
+++ tasks/yusupkina_m_elem_vec_sum/mpi/src/ops_mpi.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <vector>
 
+
 #include "yusupkina_m_elem_vec_sum/common/include/common.hpp"
 
 namespace yusupkina_m_elem_vec_sum {
@@ -30,39 +31,44 @@ bool YusupkinaMElemVecSumMPI::RunImpl() {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &count);
 
-  const auto &input_vec = GetInput();
-  int vec_size = static_cast<int>(input_vec.size());
+  int vec_size = 0;
+
+  if (rank == 0) {
+    vec_size = static_cast<int>(GetInput().size());
+  }
+  MPI_Bcast(&vec_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (vec_size == 0) {
     GetOutput() = 0;
     return true;
   }
 
-  if (count > vec_size) {
-    if (rank == 0) {
-      GetOutput() = std::accumulate(input_vec.begin(), input_vec.end(), 0LL);
-    }
-    OutType output = GetOutput();
-    MPI_Bcast(&output, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
-    GetOutput() = output;
-    return true;
-  }
 
   int base_size = vec_size / count;
   int remainder = vec_size % count;
   int cur_size = base_size + (rank < remainder ? 1 : 0);
 
-  int local_start_ind = 0;
-  for (int i = 0; i < rank; i++) {
-    local_start_ind += base_size + (i < remainder ? 1 : 0);
+  std::vector<int> sendcounts(count);
+  std::vector<int> displs(count);
+  int start_ind = 0;
+  for (int i = 0; i < count; ++i) {
+      sendcounts[i] = base_size + (i < remainder ? 1 : 0);
+      displs[i] = start_ind;
+      start_ind += sendcounts[i];
   }
 
-  std::vector<int> local_vec_part(cur_size);
-  for (int i = 0; i < cur_size; i++) {
-    local_vec_part[i] = input_vec[local_start_ind + i];
+  int* sendbuf = nullptr;
+  if (rank == 0) {
+      sendbuf = GetInput().data();
   }
+  std::vector<int> local_data(cur_size);
+  MPI_Scatterv(sendbuf, sendcounts.data(), displs.data(), MPI_INT, local_data.data(), cur_size, MPI_INT, 0, MPI_COMM_WORLD);
 
-  OutType local_sum = std::accumulate(local_vec_part.begin(), local_vec_part.end(), 0LL);
+
+  OutType local_sum = 0LL;
+  for (int i = 0; i < cur_size; ++i) {
+    local_sum += static_cast<OutType>(local_data[i]);
+  }
   OutType global_sum = 0LL;
 
   MPI_Allreduce(&local_sum, &global_sum, 1, MPI_INT64_T, MPI_SUM, MPI_COMM_WORLD);

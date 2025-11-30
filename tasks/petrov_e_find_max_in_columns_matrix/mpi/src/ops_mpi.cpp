4f67f68d82195790d16e1bcdffe1bb4f042f5906
CoolEgor61 <vip.Mazda003@bk.ru>
2025-11-30 22:46:04 +0300

    fixing pre-commit

--- tasks/petrov_e_find_max_in_columns_matrix/mpi/src/ops_mpi.cpp
+++ tasks/petrov_e_find_max_in_columns_matrix/mpi/src/ops_mpi.cpp
@@ -55,18 +55,30 @@ bool PetrovEFindMaxInColumnsMatrixMPI::RunImpl() {
   int col_num_wo_proc = m % proc_num;
   int flag = 0;
 
-  if (proc_rank < col_num_wo_proc) {
-    flag = 1;
-  } else {
-    flag = 0;
-  }
+  std::vector<int> start(proc_num);
+  std::vector<int> end(proc_num);
 
-  int start = (proc_rank * col_num_per_proc) + std::min(proc_rank, col_num_wo_proc);
-  int end = start + col_num_per_proc + flag;
+  int proc_start = 0;
+  int proc_end = 0;
+
+  if (proc_rank == 0) {
+    for (i = 0; i < proc_num; i++) {
+      if (i < col_num_wo_proc) {
+        flag = 1;
+      } else {
+        flag = 0;
+      }
+      start[i] = (i * col_num_per_proc) + std::min(i, col_num_wo_proc);
+      end[i] = start[i] + col_num_per_proc + flag;
+    }
+  }
+  
+  MPI_Scatter(start.data(), 1, MPI_INT, &proc_start, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter(end.data(), 1, MPI_INT, &proc_end, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   OutType proc_res(m, std::numeric_limits<MatrixElemType>::lowest());
 
-  for (i = start; std::cmp_less(i, end); i++) {
+  for (i = proc_start; std::cmp_less(i, proc_end); i++) {
     max = matrix[static_cast<int>(i * n)];
     for (j = 1; j < n; j++) {
       max = std::max(matrix[(i * n) + j], max);

8137d8ee8fd82daf8d2b55bd3108d204376960b1
CoolEgor61 <vip.Mazda003@bk.ru>
2025-12-01 00:54:04 +0300

    fixing pre-commit try 5

--- tasks/petrov_e_find_max_in_columns_matrix/common/include/common.hpp
+++ tasks/petrov_e_find_max_in_columns_matrix/common/include/common.hpp
@@ -12,4 +12,4 @@ using OutType = std::vector<double>;
 using TestType = std::tuple<int, int, std::vector<double>, std::vector<double>>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
-} // namespace petrov_e_find_max_in_columns_matrix
+}  // namespace petrov_e_find_max_in_columns_matrix
